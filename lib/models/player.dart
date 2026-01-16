8d676a3e2d2e923ac03de4fd6ff10523b7840442
Joaquin Galang <68993718+joaquingalang@users.noreply.github.com>
2026-01-16 23:52:54 +0800

    Add: player model

---
+++ lib/models/player.dart
@@ -0,0 +1,25 @@
+import 'package:pocket_mafia/enums/roles.dart';
+import 'package:pocket_mafia/utils/string_helpers.dart';
+
+class Player {
+
+  Player({required this.name, required this.role});
+
+  final String name;
+  final Role role;
+
+  String get roleName {
+    String roleName = '';
+    for (int i = 0; i < role.name.length; i++) {
+      if (i < 0) {
+        roleName += role.name[i].toUpperCase();
+        continue;
+      }
+      if (isUppercase(role.name[i])) {
+        roleName += ' ${role.name[i].toUpperCase()}';
+      }
+    }
+    return roleName;
+  }
+
+}
