45d8b95bbb87c8918651a66afc17d5235ce6943c
Joaquin Galang <68993718+joaquingalang@users.noreply.github.com>
2026-01-19 08:58:12 +0800

    Update: app name

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:pocket_mafia/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const PocketMafia());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
