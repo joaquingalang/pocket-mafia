40b7ff17c6bc9b1617566c25b195875c7a336330
Joaquin Galang <68993718+joaquingalang@users.noreply.github.com>
2026-01-26 15:46:37 +0800

    Update: completed visuals

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:pocket_mafia/components/rounded_rectangle_button.dart';
+import 'package:pocket_mafia/theme.dart';
+import 'package:sizer/sizer.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final theme = Theme.of(context);
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+
+              // Offset
+              const SizedBox(height: 24),
+
+              Text('Pocket Mafia', style: theme.textTheme.displaySmall),
+              Text('MODERATOR TOOL', style: theme.textTheme.labelMedium),
+
+              // Offset
+              const SizedBox(height: 56),
+
+              Image.asset('assets/images/placeholder.png', width: 250),
+
+              const SizedBox(height: 64),
+
+              RoundedRectangleButton(label: 'PLAY', onPressed: () {}),
+              RoundedRectangleButton(label: 'HOW TO PLAY', onPressed: () {}),
+              RoundedRectangleButton(label: 'SETTINGS', onPressed: () {}),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
