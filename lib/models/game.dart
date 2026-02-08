8b016d980eac70e313188f9228fb362e577a27ec
Joaquin Galang <68993718+joaquingalang@users.noreply.github.com>
2026-02-08 22:27:21 +0800

    Add: game model

---
+++ lib/models/game.dart
@@ -0,0 +1,30 @@
+import 'package:pocket_mafia/models/player.dart';
+import 'package:pocket_mafia/models/role.dart';
+
+class Game {
+  final Duration? dayDuration;
+  final Duration? nightDuration;
+  final Duration? voteDuration;
+  final List<Player>? players;
+
+  const Game({
+    this.dayDuration,
+    this.nightDuration,
+    this.voteDuration,
+    this.players,
+  });
+
+  Game copyWith({
+    Duration? dayDuration,
+    Duration? nightDuration,
+    Duration? voteDuration,
+    List<Player>? players,
+  }) {
+    return Game(
+      dayDuration: dayDuration ?? this.dayDuration,
+      nightDuration: nightDuration ?? this.nightDuration,
+      voteDuration: voteDuration ?? this.voteDuration,
+      players: players ?? this.players,
+    );
+  }
+}
