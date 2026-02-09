760b7caee1f9f67a462956a756291f886835fe57
Joaquin Galang <68993718+joaquingalang@users.noreply.github.com>
2026-02-09 13:46:34 +0800

    Update: visuals & logic

--- lib/pages/day_page.dart
+++ lib/pages/day_page.dart
@@ -1,5 +1,13 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
+import 'package:pocket_mafia/components/game_app_bar.dart';
+import 'package:pocket_mafia/components/player_tile.dart';
 import 'package:pocket_mafia/models/game.dart';
+import 'package:pocket_mafia/theme.dart';
+import 'package:sizer/sizer.dart';
 
 class DayPage extends StatefulWidget {
   const DayPage({super.key, required this.game});
@@ -13,6 +21,205 @@ class DayPage extends StatefulWidget {
 class _DayPageState extends State<DayPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final theme = Theme.of(context);
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30),
+          child: Column(
+            children: [
+              GameAppBar(
+                title: 'Day Discussion',
+                subtitle: 'DAY 1',
+                onClose: () {},
+                onInfo: () {},
+              ),
+
+              // Offset
+              SizedBox(height: 20),
+
+              PhaseTimer(
+                duration: widget.game.dayDuration!,
+                onTimeout: () {
+                  print('TIMEOUT!!!!');
+                },
+              ),
+
+              // Offset
+              SizedBox(height: 20),
+
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'SURVIVING PLAYERS (8)',
+                  style: theme.textTheme.labelSmall,
+                ),
+              ),
+
+              // Offset
+              SizedBox(height: 10),
+
+              Expanded(
+                child: ListView.builder(
+                  itemCount: widget.game.players!.length,
+                  itemBuilder: (context, index) {
+                    return PlayerTile(
+                      name: widget.game.players![index].name,
+                      id: index + 1,
+                    );
+                  },
+                ),
+              ),
+
+              // Offset
+              SizedBox(height: 20),
+
+              Row(
+                spacing: 12,
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: theme.colorScheme.secondary,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        spacing: 5,
+                        children: [
+                          Text('PAUSE', style: theme.textTheme.labelLarge),
+                          Icon(
+                            Icons.pause,
+                            color: TextColors.primaryText,
+                            size: 20,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+
+                  Expanded(
+                    child: Container(
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: theme.colorScheme.onSecondary,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        spacing: 5,
+                        children: [
+                          Text(
+                            'VOTE',
+                            style: theme.textTheme.labelLarge!.copyWith(
+                              color: theme.colorScheme.primary,
+                            ),
+                          ),
+                          SvgPicture.asset(
+                            'assets/images/icons/scales_icon.svg',
+                            width: 18,
+                            color: theme.colorScheme.primary,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+              // Offset
+              SizedBox(height: 20),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class PhaseTimer extends StatefulWidget {
+  const PhaseTimer({
+    super.key,
+    required this.duration,
+    required this.onTimeout,
+  });
+
+  final Duration duration;
+  final VoidCallback onTimeout;
+
+  @override
+  State<PhaseTimer> createState() => _PhaseTimerState();
+}
+
+class _PhaseTimerState extends State<PhaseTimer> {
+  Timer? _secondsTimer;
+  Timer? _timer;
+  int _totalSeconds = 0;
+  int _seconds = 0;
+
+  void _startTimer() {
+    _totalSeconds = widget.duration.inSeconds;
+    _seconds = widget.duration.inSeconds;
+    _secondsTimer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        _seconds -= 1;
+      });
+    });
+    _timer = Timer.periodic(widget.duration + Duration(seconds: 1), (timer) {
+      widget.onTimeout();
+      _timer!.cancel();
+      _secondsTimer!.cancel();
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _startTimer();
+  }
+
+  @override
+  void dispose() {
+    _secondsTimer!.cancel();
+    super.dispose();
+  }
+
+  String _formatDurationMMSS() {
+    final minutes = _seconds ~/ 60;
+    final seconds = _seconds % 60;
+
+    return '${minutes.toString().padLeft(2, '0')}:'
+        '${seconds.toString().padLeft(2, '0')}';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return CircularPercentIndicator(
+      radius: 20.w,
+      lineWidth: 8,
+      percent: _seconds / _totalSeconds,
+      reverse: true,
+      animation: true,
+      animateFromLastPercent: true,
+      animationDuration: 1000,
+      circularStrokeCap: CircularStrokeCap.round,
+      backgroundColor: theme.colorScheme.secondary,
+      progressColor: theme.colorScheme.onSecondary,
+      center: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            _formatDurationMMSS(),
+            style: theme.textTheme.headlineLarge!.copyWith(
+              color: theme.colorScheme.onSecondary,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text('REMAINING', style: theme.textTheme.labelSmall),
+        ],
+      ),
+    );
   }
 }
