4fe7f99cb078c9fad4596be0a62be08288c86380
Joaquin Galang <68993718+joaquingalang@users.noreply.github.com>
2026-02-09 11:28:23 +0800

    Update: game summary logic

--- lib/pages/game_summary_page.dart
+++ lib/pages/game_summary_page.dart
@@ -4,18 +4,66 @@ import 'package:pocket_mafia/components/main_app_bar.dart';
 import 'package:pocket_mafia/components/rounded_rectangle_button.dart';
 import 'package:pocket_mafia/enums/phase.dart';
 import 'package:pocket_mafia/enums/roles.dart';
+import 'package:pocket_mafia/models/game.dart';
+import 'package:pocket_mafia/models/player.dart';
 import 'package:pocket_mafia/pages/game_summary_page.dart';
+import 'package:pocket_mafia/pages/role_reveal_page.dart';
 import 'package:pocket_mafia/theme.dart';
 import 'package:pocket_mafia/utils/string_helpers.dart';
 
 class GameSummaryPage extends StatefulWidget {
-  const GameSummaryPage({super.key});
+  const GameSummaryPage({super.key, required this.game});
+
+  final Game game;
 
   @override
   State<GameSummaryPage> createState() => _GameSummaryPageState();
 }
 
 class _GameSummaryPageState extends State<GameSummaryPage> {
+  Widget _buildPlayerTiles() {
+    List<Widget> playerTiles = [];
+    final int playerCount = widget.game.players!.length;
+    final int visible = (playerCount <= 5) ? playerCount : 5;
+    for (int i = 0; i < visible; i++) {
+      final player = widget.game.players![i];
+      playerTiles.add(_PlayerTile(name: player.name, id: i + 1));
+    }
+    if (playerCount - visible > 0) {
+      playerTiles.add(_MorePlayersTile(count: playerCount - visible));
+    }
+    return Column(children: playerTiles);
+  }
+
+  Widget _buildRoleTiles() {
+    List<Widget> roleTiles = [];
+    List<Roles> roles = [];
+    for (Player player in widget.game.players!) {
+      final role = player.role.type;
+      roles.add(role);
+    }
+    for (Roles role in Roles.values) {
+      if (roles.contains(role)) {
+        roleTiles.add(
+          _RoleTile(
+            role: role,
+            count: roles.where((type) => type == role).length,
+          ),
+        );
+      }
+    }
+    return Column(children: roleTiles);
+  }
+
+  void _startGame() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => RoleRevealPage(game: widget.game),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
@@ -28,84 +76,62 @@ class _GameSummaryPageState extends State<GameSummaryPage> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 MainAppBar(label: 'Game Summary'),
-            
+
                 // Offset
                 SizedBox(height: 35),
-            
+
                 Text('PHASE SETTINGS', style: theme.textTheme.labelSmall),
-            
+
                 // Offset
                 SizedBox(height: 15),
-            
+
                 Row(
                   spacing: 10,
                   children: [
-                    _PhaseDurationTile(phase: Phase.day),
-                    _PhaseDurationTile(phase: Phase.night),
-                    _PhaseDurationTile(phase: Phase.vote),
+                    _PhaseDurationTile(
+                      phase: Phase.day,
+                      duration: widget.game.dayDuration!,
+                    ),
+                    _PhaseDurationTile(
+                      phase: Phase.night,
+                      duration: widget.game.nightDuration!,
+                    ),
+                    _PhaseDurationTile(
+                      phase: Phase.vote,
+                      duration: widget.game.voteDuration!,
+                    ),
                   ],
                 ),
-            
+
                 // Offset
                 SizedBox(height: 25),
-            
+
                 Text('PLAYERS', style: theme.textTheme.labelSmall),
-            
+
                 // Offset
                 SizedBox(height: 15),
-            
-                _PlayerTile(name: 'Alex Carter', id: 1),
-                _PlayerTile(name: 'Alex Carter', id: 2),
-                _PlayerTile(name: 'Alex Carter', id: 3),
-                _PlayerTile(name: 'Alex Carter', id: 4),
-            
-                Container(
-                  padding: EdgeInsets.all(15),
-                  margin: EdgeInsets.only(bottom: 8),
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    color: theme.colorScheme.onPrimary,
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: Text(
-                    '+6 more players',
-                    style: theme.textTheme.bodyLarge,
-                  ),
-                ),
-            
+
+                _buildPlayerTiles(),
+
                 // Offset
                 SizedBox(height: 25),
-            
+
                 Text('SELECTED ROLES', style: theme.textTheme.labelSmall),
-            
+
                 // Offset
                 SizedBox(height: 15),
-            
-                _RoleTile(
-                  role: Roles.mafia,
-                  count: 2,
-                ),
-                _RoleTile(
-                  role: Roles.detective,
-                  count: 1,
-                ),
-                _RoleTile(
-                  role: Roles.doctor,
-                  count: 1,
-                ),
-            
+
+                _buildRoleTiles(),
+
                 // Offset
                 SizedBox(height: 35),
-            
+
                 RoundedRectangleButton(
                   label: 'START GAME',
                   iconData: Icons.play_arrow_outlined,
-                  onPressed: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => GameSummaryPage()),
-                  ),
+                  onPressed: _startGame,
                 ),
-            
+
                 // Offset
                 SizedBox(height: 20),
               ],
@@ -117,12 +143,29 @@ class _GameSummaryPageState extends State<GameSummaryPage> {
   }
 }
 
+class _MorePlayersTile extends StatelessWidget {
+  const _MorePlayersTile({super.key, required this.count});
+
+  final int count;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Container(
+      padding: EdgeInsets.all(15),
+      margin: EdgeInsets.only(bottom: 8),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: theme.colorScheme.onPrimary,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Text('+$count more players', style: theme.textTheme.bodyLarge),
+    );
+  }
+}
+
 class _RoleTile extends StatelessWidget {
-  const _RoleTile({
-    super.key,
-    required this.role,
-    required this.count,
-  });
+  const _RoleTile({super.key, required this.role, required this.count});
 
   final Roles role;
   final int count;
@@ -145,7 +188,7 @@ class _RoleTile extends StatelessWidget {
             width: 43,
             height: 43,
             decoration: BoxDecoration(
-              color: const Color(0xFFF43F5E),
+              color: roleColor[role],
               borderRadius: BorderRadius.circular(8),
             ),
           ),
@@ -162,7 +205,7 @@ class _RoleTile extends StatelessWidget {
               Text(
                 (role == Roles.mafia) ? 'KILLER TEAM' : 'VILLAGER TEAM',
                 style: theme.textTheme.labelSmall!.copyWith(
-                  color: const Color(0xFFF43F5E),
+                  color: roleColor[role],
                 ),
               ),
             ],
@@ -174,14 +217,14 @@ class _RoleTile extends StatelessWidget {
             width: 36,
             height: 36,
             decoration: BoxDecoration(
-              color: const Color(0xFFF43F5E).withOpacity(0.25),
+              color: roleColor[role]!.withOpacity(0.25),
               borderRadius: BorderRadius.circular(1000),
             ),
             child: Center(
               child: Text(
-                '3',
+                count.toString(),
                 style: theme.textTheme.labelLarge!.copyWith(
-                  color: const Color(0xFFF43F5E),
+                  color: roleColor[role],
                 ),
               ),
             ),
@@ -226,9 +269,14 @@ class _PlayerTile extends StatelessWidget {
 }
 
 class _PhaseDurationTile extends StatelessWidget {
-  const _PhaseDurationTile({super.key, required this.phase});
+  const _PhaseDurationTile({
+    super.key,
+    required this.phase,
+    required this.duration,
+  });
 
   final Phase phase;
+  final Duration duration;
 
   String getPhaseIcon() {
     switch (phase) {
@@ -261,7 +309,7 @@ class _PhaseDurationTile extends StatelessWidget {
                 color: const Color(0xFF4E5F79),
               ),
             ),
-            Text('300S', style: theme.textTheme.labelLarge),
+            Text('${duration.inSeconds}S', style: theme.textTheme.labelLarge),
           ],
         ),
       ),
