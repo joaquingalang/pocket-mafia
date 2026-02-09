f553733bb9824122fc0a0867d9c65c5c5e1cc037
Joaquin Galang <68993718+joaquingalang@users.noreply.github.com>
2026-02-09 11:27:07 +0800

    Update: disabled field

--- lib/components/rounded_rectangle_button.dart
+++ lib/components/rounded_rectangle_button.dart
@@ -7,11 +7,13 @@ class RoundedRectangleButton extends StatefulWidget {
     required this.label,
     this.iconData,
     required this.onPressed,
+    this.isDisabled = false,
   });
 
   final String label;
   final IconData? iconData;
   final VoidCallback onPressed;
+  final bool isDisabled;
 
   @override
   State<RoundedRectangleButton> createState() => _RoundedRectangleButtonState();
@@ -22,6 +24,7 @@ class _RoundedRectangleButtonState extends State<RoundedRectangleButton> {
   bool _isPressed = false;
 
   Future<void> _handlePress(bool pressed) async {
+    if (widget.isDisabled) return;
     setState(() {
       _isPressed = pressed;
     });
@@ -36,43 +39,47 @@ class _RoundedRectangleButtonState extends State<RoundedRectangleButton> {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     return AnimatedOpacity(
-      opacity: _isPressed ? 0.5 : 1.0,
-      duration: const Duration(milliseconds: 100),
-      child: GestureDetector(
-        onTapDown: (details) => _handlePress(true),
-        onTapUp: (details) => _handlePress(false),
-        child: Container(
-          width: double.infinity,
-          height: 50,
-          padding: EdgeInsets.symmetric(vertical: 15),
-          decoration: BoxDecoration(
-            color: theme.colorScheme.onSecondary,
-            borderRadius: BorderRadius.circular(12),
-            boxShadow: [
-              if (!_isPressed) BoxShadow(
-                offset: Offset(0, 2),
-                blurRadius: 10,
-                color: theme.colorScheme.onSecondary.withOpacity(0.5),
-              )
-            ],
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            spacing: 5,
-            children: [
-              Text(
-                widget.label,
-                style: theme.textTheme.labelLarge!.copyWith(
-                  color: TextColors.buttonText,
+      opacity: widget.isDisabled ? 0.25 : 1,
+      duration: const Duration(milliseconds: 200),
+      child: AnimatedOpacity(
+        opacity: _isPressed ? 0.5 : 1.0,
+        duration: const Duration(milliseconds: 100),
+        child: GestureDetector(
+          onTapDown: (details) => _handlePress(true),
+          onTapUp: (details) => _handlePress(false),
+          child: Container(
+            width: double.infinity,
+            height: 50,
+            padding: EdgeInsets.symmetric(vertical: 15),
+            decoration: BoxDecoration(
+              color: theme.colorScheme.onSecondary,
+              borderRadius: BorderRadius.circular(12),
+              boxShadow: [
+                if (!_isPressed) BoxShadow(
+                  offset: Offset(0, 2),
+                  blurRadius: 10,
+                  color: theme.colorScheme.onSecondary.withOpacity(0.5),
+                )
+              ],
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              spacing: 5,
+              children: [
+                Text(
+                  widget.label,
+                  style: theme.textTheme.labelLarge!.copyWith(
+                    color: TextColors.buttonText,
+                  ),
                 ),
-              ),
-              if (widget.iconData != null) Icon(
-                widget.iconData,
-                color: theme.colorScheme.primary,
-                size: 24,
-              )
-            ],
+                if (widget.iconData != null) Icon(
+                  widget.iconData,
+                  color: theme.colorScheme.primary,
+                  size: 24,
+                )
+              ],
+            ),
           ),
         ),
       ),
