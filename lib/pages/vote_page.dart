48be809e7f681b18a92fa403bb8238f4ed0d9d07
Joaquin Galang <68993718+joaquingalang@users.noreply.github.com>
2026-02-10 13:58:44 +0800

    Update: voting to vote page

---
+++ lib/pages/vote_page.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:pocket_mafia/components/game_app_bar.dart';
+import 'package:pocket_mafia/components/phase_timer.dart';
+import 'package:pocket_mafia/components/player_select_tile.dart';
+import 'package:pocket_mafia/components/player_tile.dart';
+import 'package:pocket_mafia/models/game.dart';
+import 'package:pocket_mafia/theme.dart';
+
+class VotePage extends StatefulWidget {
+  const VotePage({super.key, required this.game});
+
+  final Game game;
+
+  @override
+  State<VotePage> createState() => _VotePageState();
+}
+
+class _VotePageState extends State<VotePage> {
+
+  bool _isSelected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30),
+          child: Column(
+            children: [
+              GameAppBar(
+                title: 'Day Discussion',
+                subtitle: 'DAY 1',
+                onClose: () {},
+                onInfo: () {},
+              ),
+
+              // Offset
+              SizedBox(height: 20),
+
+              PhaseTimer(
+                duration: widget.game.dayDuration!,
+                onTimeout: () {
+                  print('TIMEOUT!!!!');
+                },
+              ),
+
+              // Offset
+              SizedBox(height: 20),
+
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: theme.colorScheme.secondary,
+                  borderRadius: BorderRadius.circular(12)
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text('CURRENT VOTER', style: theme.textTheme.labelSmall),
+                    Text('Alex Carter', style: theme.textTheme.headlineSmall),
+                  ],
+                ),
+              ),
+              
+              // Offset
+              SizedBox(height: 20),
+
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'SELECT A PLAYER TO LYNCH',
+                  style: theme.textTheme.labelSmall,
+                ),
+              ),
+
+              // Offset
+              SizedBox(height: 10),
+
+              Expanded(
+                child: ListView.builder(
+                  itemCount: widget.game.players!.length,
+                  itemBuilder: (context, index) {
+                    return PlayerSelectTile(
+                      name: widget.game.players![index].name,
+                      id: index + 1,
+                      value: _isSelected,
+                      onChanged: (value) => setState(() {
+                        _isSelected = value;
+                      }),
+                    );
+                  },
+                ),
+              ),
+
+              // Offset
+              SizedBox(height: 20),
+
+              Row(
+                spacing: 12,
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: theme.colorScheme.secondary,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        spacing: 5,
+                        children: [
+                          Text('PAUSE', style: theme.textTheme.labelLarge),
+                          Icon(
+                            Icons.pause,
+                            color: TextColors.primaryText,
+                            size: 20,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+
+                  Expanded(
+                    child: Container(
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: theme.colorScheme.onSecondary,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        spacing: 5,
+                        children: [
+                          Text(
+                            'VOTE',
+                            style: theme.textTheme.labelLarge!.copyWith(
+                              color: theme.colorScheme.primary,
+                            ),
+                          ),
+                          SvgPicture.asset(
+                            'assets/images/icons/scales_icon.svg',
+                            width: 18,
+                            color: theme.colorScheme.primary,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+              // Offset
+              SizedBox(height: 20),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
