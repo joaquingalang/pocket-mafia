6aa73e9e2b97bad1ab72d1bf0fe01e37a7ca1d52
Joaquin Galang <68993718+joaquingalang@users.noreply.github.com>
2026-02-12 13:52:20 +0800

    Update: completed visuals

---
+++ lib/pages/vote_result_page.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:percent_indicator/flutter_percent_indicator.dart';
+import 'package:pocket_mafia/components/rounded_rectangle_button.dart';
+import 'package:sizer/sizer.dart';
+
+class VoteResultPage extends StatelessWidget {
+  const VoteResultPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30),
+          child: Column(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text('THE EXECUTION', style: theme.textTheme.labelSmall),
+                  Text(
+                    'The Village Has Decided',
+                    style: theme.textTheme.headlineSmall,
+                  ),
+                ],
+              ),
+
+              Spacer(),
+
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(40),
+                    decoration: BoxDecoration(
+                      color: theme.colorScheme.secondary,
+                      borderRadius: BorderRadius.circular(1000),
+                    ),
+                    child: SvgPicture.asset(
+                      'assets/images/icons/dead_icon.svg',
+                    ),
+                  ),
+
+                  // Offset
+                  SizedBox(height: 30),
+
+                  Text('ALEX CARTER', style: theme.textTheme.displaySmall),
+
+                  // Offset
+                  SizedBox(height: 10),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    spacing: 6,
+                    children: [
+                      Container(
+                        height: 1,
+                        width: 46,
+                        decoration: BoxDecoration(
+                          color: theme.colorScheme.secondary,
+                          borderRadius: BorderRadius.circular(100),
+                        ),
+                      ),
+
+                      Text(
+                        'VILLAGER',
+                        style: theme.textTheme.labelSmall!.copyWith(
+                          color: theme.colorScheme.onSecondary,
+                        ),
+                      ),
+
+                      Container(
+                        height: 1,
+                        width: 46,
+                        decoration: BoxDecoration(
+                          color: theme.colorScheme.secondary,
+                          borderRadius: BorderRadius.circular(100),
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  // Offset
+                  SizedBox(height: 20),
+
+                  SizedBox(
+                    width: 60.w,
+                    child: Text(
+                      'The decision is made. Alex Carter will not see another day.',
+                      style: theme.textTheme.bodyMedium,
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+
+              Spacer(),
+
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                spacing: 8,
+                children: [
+                  Text(
+                    'PROCEEDING TO NIGHT IN 5S...',
+                    style: theme.textTheme.labelSmall,
+                  ),
+
+                  LinearProgressIndicator(
+                    value: 0.5,
+                    minHeight: 10,
+                    color: theme.colorScheme.onSecondary,
+                    borderRadius: BorderRadius.circular(1000),
+                  ),
+                ],
+              ),
+
+              // Offset
+              SizedBox(height: 15),
+
+              RoundedRectangleButton(
+                label: 'PROCEED TO NIGHT',
+                iconData: Icons.nightlight_outlined,
+                onPressed: () {},
+              ),
+
+              // Offset
+              SizedBox(height: 20),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
