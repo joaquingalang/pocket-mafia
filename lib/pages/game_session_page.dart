14c4968ed758bbea5c4d54f125e3741e8fc22d35
Joaquin Galang <68993718+joaquingalang@users.noreply.github.com>
2026-05-13 21:38:28 +0800

    Update: get game session phase

--- lib/pages/game_session_page.dart
+++ lib/pages/game_session_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:pocket_mafia/enums/phase.dart';
 import 'package:pocket_mafia/models/game_settings.dart';
 import 'package:pocket_mafia/models/player.dart';
+import 'package:pocket_mafia/views/day_view.dart';
+import 'package:pocket_mafia/views/night_view.dart';
+import 'package:pocket_mafia/views/voting_view.dart';
 
 class GameSessionPage extends StatefulWidget {
   const GameSessionPage({super.key, required this.settings});
@@ -18,15 +22,33 @@ class _GameSessionPageState extends State<GameSessionPage> {
   late final Duration? voteDuration;
   late final List<Player>? players;
 
+  Phase phase = Phase.day;
+  bool isVoting = false;
+
+  Widget getPhaseView() {
+    switch (phase) {
+      case Phase.day:
+        return DayView(dayDuration: dayDuration!, players: players!);
+      case Phase.night:
+        return NightView(nightDuration: nightDuration!, players: players!);
+      case Phase.voting:
+        return VotingView(voteDuration: voteDuration!, players: players!);
+    }
+  }
+
   @override
   void initState() {
     super.initState();
-    final GameSettings(:dayDuration, :nightDuration, :voteDuration, :players) = widget.settings;
+    dayDuration = widget.settings.dayDuration;
+    nightDuration = widget.settings.nightDuration;
+    voteDuration = widget.settings.voteDuration;
+    players = widget.settings.players;
   }
 
   @override
   Widget build(BuildContext context) {
-    print(dayDuration);
-    return const Placeholder();
+    return Scaffold(
+      body: getPhaseView(),
+    );
   }
 }
