ba19cec175f25418168cbdc72f0df792893d954c
Joaquin Galang <68993718+joaquingalang@users.noreply.github.com>
2026-05-18 18:02:27 +0800

    Update: page body

--- lib/views/night_view.dart
+++ lib/views/night_view.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:pocket_mafia/components/game_app_bar.dart';
+import 'package:pocket_mafia/components/phase_timer.dart';
+import 'package:pocket_mafia/components/player_select_tile.dart';
 import 'package:pocket_mafia/models/player.dart';
+import 'package:pocket_mafia/theme.dart';
 
 class NightView extends StatefulWidget {
   const NightView({super.key, required this.duration, required this.players});
@@ -12,8 +17,125 @@ class NightView extends StatefulWidget {
 }
 
 class _NightViewState extends State<NightView> {
+  bool _isSelected = false;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final theme = Theme.of(context);
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30),
+          child: Column(
+            children: [
+              GameAppBar(
+                title: 'Night Action',
+                subtitle: 'DAY 1',
+                onClose: () {},
+                onInfo: () {},
+              ),
+
+              // Offset
+              SizedBox(height: 20),
+
+              PhaseTimer(
+                duration: widget.duration,
+                onTimeout: () {
+                  print('TIMEOUT!!!!');
+                },
+              ),
+
+              // Offset
+              SizedBox(height: 20),
+
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                    color: theme.colorScheme.secondary,
+                    borderRadius: BorderRadius.circular(12)
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text('MAFIA', style: theme.textTheme.headlineSmall),
+                    Text('CHOOSE YOUR VICTIM', style: theme.textTheme.labelSmall),
+                  ],
+                ),
+              ),
+
+              // Offset
+              SizedBox(height: 20),
+
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'SELECT A PLAYER TO LYNCH',
+                  style: theme.textTheme.labelSmall,
+                ),
+              ),
+
+              // Offset
+              SizedBox(height: 10),
+
+              Expanded(
+                child: ListView.builder(
+                  itemCount: widget.players.length,
+                  itemBuilder: (context, index) {
+                    return PlayerSelectTile(
+                      name: widget.players[index].name,
+                      id: index + 1,
+                      value: _isSelected,
+                      onChanged: (value) => setState(() {
+                        _isSelected = value;
+                      }),
+                    );
+                  },
+                ),
+              ),
+
+              // Offset
+              SizedBox(height: 20),
+
+              Row(
+                spacing: 12,
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: theme.colorScheme.onSecondary,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        spacing: 5,
+                        children: [
+                          Text(
+                            'CONFIRM',
+                            style: theme.textTheme.labelLarge!.copyWith(
+                              color: theme.colorScheme.primary,
+                            ),
+                          ),
+                          SvgPicture.asset(
+                            'assets/images/icons/check_circle_icon.svg',
+                            width: 18,
+                            color: theme.colorScheme.primary,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+              // Offset
+              SizedBox(height: 20),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
