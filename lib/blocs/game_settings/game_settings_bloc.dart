ee52772ee9ec5f81571c1221307107ec7d6434df
Joaquin Galang <68993718+joaquingalang@users.noreply.github.com>
2026-05-30 22:27:40 +0800

    Add: init roles event handler

--- lib/blocs/game_settings/game_settings_bloc.dart
+++ lib/blocs/game_settings/game_settings_bloc.dart
@@ -2,6 +2,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pocket_mafia/blocs/game_settings/game_settings_event.dart';
 import 'package:pocket_mafia/blocs/game_settings/game_settings_state.dart';
 import 'package:pocket_mafia/enums/roles.dart';
+import 'package:pocket_mafia/models/game_settings.dart';
 
 class GameSettingsBloc extends Bloc<GameSettingsEvent, GameSettingsState> {
   GameSettingsBloc() : super(const GameSettingsState()) {
@@ -10,42 +11,73 @@ class GameSettingsBloc extends Bloc<GameSettingsEvent, GameSettingsState> {
     on<GameSettingsSetDayDuration>(_onSetDayDuration);
     on<GameSettingsSetVoteDuration>(_onSetVoteDuration);
     on<GameSettingsSetNightDuration>(_onSetNightDuration);
+    on<GameSettingsInitRoles>(_onInitRoles);
     on<GameSettingsAddRole>(_onAddRole);
     on<GameSettingsRemoveRole>(_onRemoveRole);
   }
 
-  void _onAddPlayer(GameSettingsAddPlayer event,
-      Emitter<GameSettingsState> emit) {
+  void _onAddPlayer(
+    GameSettingsAddPlayer event,
+    Emitter<GameSettingsState> emit,
+  ) {
     emit(state.copyWith(names: [event.name, ...state.names]));
   }
 
-  void _onRemovePlayer(GameSettingsRemovePlayer event,
-      Emitter<GameSettingsState> emit) {
-    emit(state.copyWith(names: List<String>.from(state.names)
-      ..removeAt(event.index)));
+  void _onRemovePlayer(
+    GameSettingsRemovePlayer event,
+    Emitter<GameSettingsState> emit,
+  ) {
+    emit(
+      state.copyWith(
+        names: List<String>.from(state.names)..removeAt(event.index),
+      ),
+    );
   }
 
-  void _onSetDayDuration(GameSettingsSetDayDuration event,
-      Emitter<GameSettingsState> emit) {
+  void _onSetDayDuration(
+    GameSettingsSetDayDuration event,
+    Emitter<GameSettingsState> emit,
+  ) {
     emit(state.copyWith(dayDuration: event.duration));
   }
 
-  void _onSetVoteDuration(GameSettingsSetVoteDuration event,
-      Emitter<GameSettingsState> emit) {
+  void _onSetVoteDuration(
+    GameSettingsSetVoteDuration event,
+    Emitter<GameSettingsState> emit,
+  ) {
     emit(state.copyWith(voteDuration: event.duration));
   }
 
-  void _onSetNightDuration(GameSettingsSetNightDuration event,
-      Emitter<GameSettingsState> emit) {
+  void _onSetNightDuration(
+    GameSettingsSetNightDuration event,
+    Emitter<GameSettingsState> emit,
+  ) {
     emit(state.copyWith(nightDuration: event.duration));
+  }
+
+  void _onInitRoles(
+    GameSettingsInitRoles event,
+    Emitter<GameSettingsState> emit,
+  ) {
+    final mafiaCount = ((state.names.length + 1) / 4).floor().clamp(
+      1,
+      state.names.length - 1,
+    );
+    List<Roles> mafiaList = [Roles.detective, Roles.doctor];
+    for (int i = 0; i < mafiaCount; i++) {
+      mafiaList.add(Roles.mafia);
+    }
+    emit(state.copyWith(roles: [...mafiaList, ...state.roles]));
   }
 
   void _onAddRole(GameSettingsAddRole event, Emitter<GameSettingsState> emit) {
     emit(state.copyWith(roles: [...state.roles, event.role]));
   }
 
-  void _onRemoveRole(GameSettingsRemoveRole event,
-      Emitter<GameSettingsState> emit) {
+  void _onRemoveRole(
+    GameSettingsRemoveRole event,
+    Emitter<GameSettingsState> emit,
+  ) {
     List<Roles> deleted = List<Roles>.from(state.roles);
     for (int i = 0; i < deleted.length; i++) {
       if (deleted[i] == event.role) {
@@ -55,5 +87,4 @@ class GameSettingsBloc extends Bloc<GameSettingsEvent, GameSettingsState> {
     }
     emit(state.copyWith(roles: deleted));
   }
-
 }
