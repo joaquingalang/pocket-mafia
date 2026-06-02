9e551385f2f7ab5ae2115b79e2d16c1657b7c867
Joaquin Galang <68993718+joaquingalang@users.noreply.github.com>
2026-06-02 13:21:37 +0800

    Add: isDeceased field

--- lib/models/player.dart
+++ lib/models/player.dart
@@ -3,10 +3,19 @@ import 'package:pocket_mafia/models/role.dart';
 
 class Player extends Equatable {
 
-  Player({required this.name, required this.role});
+  Player({required this.name, required this.role, this.isDeceased=false});
 
   final String name;
   final Role role;
+  final bool isDeceased;
+
+  Player copyWith({String? name, Role? role, bool? isDeceased}) {
+    return Player(
+      name: name ?? this.name,
+      role: role ?? this.role,
+      isDeceased: isDeceased ?? this.isDeceased,
+    );
+  }
 
   @override
   List<Object?> get props => [name, role];
